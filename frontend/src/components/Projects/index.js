2b6437517dd945447b1157e780c31216f779d8f2
Danielle Regan Arnett <126263892+DReganArnett@users.noreply.github.com>
2023-11-03 12:07:15 -0600

    Added projects page, and footer throughout

--- frontend/src/components/Projects/index.js
+++ frontend/src/components/Projects/index.js
@@ -3,7 +3,9 @@ import './index.scss';
 import Loader from 'react-loaders';
 import Sidebar from '../Sidebar';
 import AnimatedLetters from '../AnimatedLetters';
-import Lunchbox from '../../assets/images/lunchbox.jpg'
+import Lunchbox from '../../assets/images/lunchbox.jpg';
+import GoldenRetriever from '../../assets/images/golden_retriever.png';
+import THDLogo from '../../assets/images/medium-transparent-logo.png';
 
 const Projects = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -12,7 +14,6 @@ const Projects = () => {
         <>
             <div className='container projects-page'>
                 <Sidebar />
-                
                 <div className='text-zone'>
                     <h1>
                         <AnimatedLetters 
@@ -20,22 +21,65 @@ const Projects = () => {
                             strArray={["M","y", " ", "P", "r", "o", "j", "e", "c", "t", "s", " ", ":"]}
                             idx={15}
                         />
-                    </h1>   
+                    </h1>  
                     <div className='MKL'>
-                    <a href='https://my-kids-lunchbox.onrender.com'><img className='lunchbox' src={Lunchbox} /></a>
-                        <br/><br/><br/>
-                    <a href='https://my-kids-lunchbox.onrender.com'><p className='MKL-title'>My Kids' Lunchbox</p></a>
-                    <p>My Kids' Lunchbox is a dynamic web application created to help families manage what foods they put into children's lunchboxes. 
-                    Many families find it challenging to continuously pack lunches that their child will eat at school.  <br/> 
-                    
-                    <br/>Many parents feel frustrated 
-                    when they open their child's lunchbox in the evening only to discover that only a few bites were taken out of a couple of items. 
-                    With lunch periods being the main time of day for children to socialize, many are distracted and either forget to eat or are not
-                    excited enough about the items in their lunchbox to stop talking long enough to eat. <br/>
-                    
-                    <br/>My Kids' Lunchbox is meant to
-                    be a place where families can share ideas for meals that are nutritious, convenient, and that kids want to eat.</p>    
+                        <a href='https://my-kids-lunchbox.onrender.com'><img className='lunchbox' src={Lunchbox} /></a>
+                            <br/><br/><br/>
+                        <a href='https://my-kids-lunchbox.onrender.com'><p className='MKL-title'>My Kids' Lunchbox</p></a>
+                        <text className='technologies'> 
+                                <ul>
+                                    <li>JavaScript</li>
+                                    <li>CSS</li>
+                                    <li>Node.JS</li>
+                                    <li>Express.JS</li>
+                                    <li>React.JS</li>
+                                    <li>PostgreSQL</li>
+                                </ul> 
+                        </text>
+                        <p>My Kids' Lunchbox is a dynamic web application created to help families manage what foods they put into children's lunchboxes.</p> 
                     </div>
+
+                    <br /><br /><br/>
+
+                    <div className='puppy-match'>
+                        <a href='https://deregan54.github.io/Memory-Game/'><img className='goldenRetriever' src={GoldenRetriever} /></a>
+                        <a href='https://deregan54.github.io/Memory-Game/'><p className='puppyMatch-title'>Puppy Match</p></a>
+                        <text className='technologies'> 
+                            <ul>
+                                <li>HTML</li> 
+                                <li>CSS</li>
+                                <li>JavaScript</li>
+                            </ul> 
+                        </text>
+                        <p>Puppy Match is a basic memory-match game.
+                        This is the very first application I created, back in September of 2022.  
+                        As you can see, I've come a long way since then! 
+                        </p> 
+                   </div>
+
+                   <br/><br/><br/>
+
+                    <div className='THD'>
+                        <img className='THDLogo' src={THDLogo}></img>
+                        <a><p className='THD-title'>The Blog</p></a>
+                        <text className='technologies'> 
+                            <ul>
+                                <li>JavaScript</li>
+                                <li>Node.JS</li>
+                                <li>Express.JS</li>
+                                <li>React.JS</li>
+                                <li>MaterialUI</li>
+                                <li>PostgreSQL</li>
+                            </ul> 
+                        </text>
+                        <p>Coming Soon! 
+                            <br/><br/>
+                           The Humble Developer's Blog will chronicle my journey into the world of software development.
+                           Like most new developers, I encounter challenges and breakthroughs daily.  Through this blog, 
+                           I plan on celebrating the breakthroughs and sharing my approach to tackling the challenges
+                           head-on.
+                        </p> 
+                   </div>
                 </div>
             </div>
             <Loader type='pacman' />
